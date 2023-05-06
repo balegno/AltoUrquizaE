490a0df85f670fa3a8a1f6a08802460a050c90fc
balegno <mrtinblegno@gmail.com>
2023-05-06 18:19:54 -0300

    Agregando map, img e iconos

---
+++ class.js
@@ -0,0 +1,19 @@
+// primer ciclo
+
+// for(let i= 0; i <=12;i = i +1){
+//     console.log(`valor interacion ${i}`)
+// }
+
+
+// let numeroIngresado = parseInt(prompt("ingrese el numero que desea saber la tabla"))
+// for(let i=0; i<=10; i++){
+//     console.log(`${numeroIngresado} x ${i} = ${numeroIngresado * i}`)
+// }
+
+// let cantidadNotas = parseInt(prompt("ingrese cantidad de notas"))
+// let total=0
+// for( let i=1; i<=cantidadNotas; i++){
+//     let nota = parseInt(prompt("ingrese nota "))
+//     total = total + nota
+//     console.log(`El total parcial es ${total}`)
+// }
